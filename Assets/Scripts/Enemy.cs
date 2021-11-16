2cfbf52cf95b854719c3a42924674e3ff90fd4c6
hongnv <hongnv@iecgames.net>
2021-11-16 23:46:06 +0700

    Add sound

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,11 @@
 public class Enemy : MonoBehaviour
 {
     private float health = 100f;
+    [SerializeField] private AudioSource bloodHit;
 
     public void TakeDamage(float damage)
     {
+        bloodHit.Play();
         health -= damage;
         if (health < 0f)
         {
--- Assets/Scripts/ShootEnemy.cs
+++ Assets/Scripts/ShootEnemy.cs
@@ -5,17 +5,25 @@ public class ShootEnemy : MonoBehaviour
 {
     public Button shootButton;
     public Camera fpsCam;
-    private float damage;
+    public int forceAdd = 300;
+    public float damage;
     [SerializeField] private GameObject bloodEffect;
     [SerializeField] private GameObject shootingEffect;
+
+    private AudioSource fireSound;
+    private AudioSource reloadSound;
 
     private void Start()
     {
         shootButton.onClick.AddListener(OnShoot);
+        AudioSource[] sounds = GetComponents<AudioSource>();
+        fireSound = sounds[0];
+        reloadSound = sounds[1];
     }
 
     private void OnShoot()
     {
+        fireSound.Play();
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
         {
@@ -30,6 +38,11 @@ public class ShootEnemy : MonoBehaviour
             {
                 var shootEffectGO = Instantiate(shootingEffect, hit.point, Quaternion.LookRotation(hit.normal));
                 Destroy(shootEffectGO, 0.2f);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(hit.normal * forceAdd);
             }
         }
     }
